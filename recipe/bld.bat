0d7127cf849315da7a0f759990efb2b7bbec1cc3
Lorenzo Pirritano <lpirritano@anaconda.com>
2024-12-11 11:16:13 +0100

    geos 3.10.6

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,15 +1,10 @@
 mkdir build
 cd build
 
-:: this is probably not the right revision.  I'm not sure how much it matters - just incorrect info.
-::    This should be generatable but requires a bash shell
-echo #define GEOS_REVISION "0" > geos_revision.h
-
-
 :: Configure.
 cmake -G "%CMAKE_GENERATOR%" ^
       -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-      -D GEOS_BUILD_STATIC=OFF ^
+      -D BUILD_SHARED_LIBS=ON ^
       -D CMAKE_BUILD_TYPE=Release ^
       %SRC_DIR%
 if errorlevel 1 exit 1
